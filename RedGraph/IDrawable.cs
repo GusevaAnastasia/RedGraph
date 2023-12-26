7b873ecb6f5187c8eb02650013f84fec79114ff9
Guseva Anastasia <92533196+GusevaAnastasia@users.noreply.github.com>
2023-12-26 21:37:01 +0500

    Add files via upload

---
+++ RedGraph/IDrawable.cs
@@ -0,0 +1,14 @@
+﻿using SFML.Graphics;
+using SFML.System;
+
+namespace RedGraph
+{
+    public interface IDrawable
+    {
+        void Draw(RenderWindow window, uint width, uint height, SFML.Graphics.Color color, float phi, float teta, float zc);
+
+        bool CheckClick(Vector2i position, uint width, uint height, float phi, float teta, float zc);
+
+        void Click();
+    }
+}
---
+++ RedGraph/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RedGraph
+{
+    public static class Program
+    {
+        [STAThread]
+        public static int Main()
+        {
+            UserInterface.Start();
+
+            return 0;
+        }
+    }
+}
